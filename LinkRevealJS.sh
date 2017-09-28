4599b469a2828ae42cf6b7bc0e753690fa3b9f99
ChenYen Lai <chengyanlai@gmail.com>
2017-09-28 14:42:54 -0600

    add convenient files

---
+++ LinkRevealJS.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -s /Volumes/Files/PublicRepo/reveal.js/css ./css
+ln -s /Volumes/Files/PublicRepo/reveal.js/js ./js
+ln -s /Volumes/Files/PublicRepo/reveal.js/lib ./lib
+ln -s /Volumes/Files/PublicRepo/reveal.js/plugin ./plugin
+ln -s /Volumes/Files/PublicRepo/reveal.js/package.json ./package.json
+ln -s /Volumes/Files/PublicRepo/reveal.js/Gruntfile.js ./Gruntfile.js
+ln -s /Volumes/Files/PublicRepo/reveal.js/node_modules ./node_modules
+ln -s /Volumes/Files/PublicRepo/MathJax ./MathJax
